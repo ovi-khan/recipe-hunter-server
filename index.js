e5fd1cf4eb6bbdf13067d8ceaabaf828edd1b848
ovi-khan <manikkhan2240@gmail.com>
2023-05-04 02:35:25 +0600

    description add

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const port = process.env.PORT || 5000;
+
+// const chefData = require('./data/chefData.json');
+
+// app.use(cors())
+
+// app.get('/', (req, res) => {
+//     res.send('data is running')
+// })
+
+// app.get('/chefData', (req, res) => {
+//     res.send(chefData)
+// })
+// app.get('/chefData/:id', (req, res) => {
+//     const id = req.params.id
+//     console.log(id)
+//     const selectChef = chefData.find(c => c.id === id)
+//     res.send(selectChef)
+// })
+
+// app.listen(port, () => {
+//     console.log(`Data Api is running on port: ${port}}`)
+// })
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -8,13 +35,24 @@ const chefData = require('./data/chefData.json');
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('data is running')
+  res.send('Data API is running')
 })
 
 app.get('/chefData', (req, res) => {
-    res.send(chefData)
+  res.send(chefData)
+})
+
+app.get('/chefData/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefData.find(chef => chef.id == id);
+
+  if (selectedChef) {
+    res.send(selectedChef);
+  } else {
+    res.status(404).send('Chef not found');
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Data Api is running on port: ${port}}`)
-})
+  console.log(`Data API is running on port: ${port}`);
+});
